ec683470c164fe1b660cf59e0cbd82ffa890801c
Darian Fultz <dfultz826@yahoo.com>
2022-08-26 11:29:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Darian",
+    streetAddress: "3050 Curtiss Ave",
+}
+function updateEmployeeWithKeyAndValue(obj, key, value){return {...obj, [key]: value};
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee [key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key]; 
+    return obj;
+}
